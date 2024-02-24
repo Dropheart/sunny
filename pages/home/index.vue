254472e0b981b9cbc282f21e9e2f06bf0f28debc
aa7123 <aa7123@imperial.ac.uk>
2024-02-24 01:41:17 +0000

    added weekdays

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,12 +1,12 @@
 <template>
 <main>
-<body class="bg-blue-bg">
+<body class="bg-blue-bg overflow-hidden">
 <div class="grid h-screen grid-cols-5 grid-rows-12">
     
     <div class="flex flex-col row-span-1 col-span-5">
         <div class="h-full grid grid-cols-5 divide-x-[1px]">
             <div class="flex w-full col-span-1">
-                <h class="text-white my-auto mx-auto text-2xl">sunny 🌻</h>
+                <h1 class="text-white my-auto mx-auto text-2xl">sunny 🌻</h1>
             </div>
             <div class="flex max-h-full col-span-4 col-start-2">
                 <div class="pl-2 my-auto">
@@ -28,8 +28,11 @@
                 <p class="text-white pl-5 pt-2 text-2xl">Everything To Do</p>
             </div>
         </div>
-        <div class="flex row-span-11 col-span-4 col-start-2">
-            <div :class="`grid grid-rows-${days.length} w-full`">
+        <div class="flex flex-col row-span-11 col-span-4 col-start-2">
+            <div class="flex">
+                <h1 class="border w-full text-center text-white py-1" v-for="day in weekDays"> {{ day }} </h1>
+            </div>
+            <div :class="`grid size-full`">
                 <div class="flex" v-for="week in days">
                     <div class="border w-full" v-for="day in week">
                         <p :class="`${day.currentMonth ? 'text-white' : 'text-grey-text'} text-right pr-2`">{{day.day}}</p> 
@@ -48,5 +51,7 @@
 const { year, month, computeDays }  = useCal(new Date())
 
 const days = computeDays()
+
+const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 </script>
