32ab766fd7472910b47f67668b6a8baf7a649a9c
aa7123 <aa7123@imperial.ac.uk>
2024-02-27 22:21:54 +0000

    text input now a two way v-model

--- components/TextInput.vue
+++ components/TextInput.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="rounded-[10px] border-4 border-blue-green">
         <input  class="w-full rounded-md bg-blue-input pl-2 font-inter placeholder-grey-text caret-grey-text text-white text-md"
-                :type="type" :id="inputName" :name="inputName" :placeholder="inputName">
+                :type="type" v-model="model" :id="inputName" :name="inputName" :placeholder="inputName">
     </div>
 </template>
 
@@ -10,5 +10,6 @@ const props = defineProps({
     inputName: String,
     type: String
 })
+const model = defineModel()
 
 </script>
