54e8eb5594cc838705b02e8db5088b8cadf59886
aa7123 <jay@abussaud.dev>
2024-04-01 17:14:35 +0100

    added name err + all use one var

--- components/LogUp.vue
+++ components/LogUp.vue
@@ -5,12 +5,7 @@
             <TextInput v-model="name" class="my-1.5 w-1/2" inputName="name" type="text" pattern="\S.*"/>
         </div>
         <div class="space-y-4">
-            <Transition>
-                <p v-if="emailError != ''" class="text-grey-text -my-2 text-red-500">{{emailError}}</p>
-            </Transition>
-            <Transition>
-                <p v-if="passwordError != ''" class="text-grey-text -my-2 text-red-500">{{passwordError}}</p>
-            </Transition>
+            <p v-if="errorText != ''" class="text-grey-text -my-2 text-red-500">{{errorText}}</p>
 
             <TextInput class="w-full" v-model="email" inputName="email" type="email"/>            
             <TextInput class="w-full" v-model="password" inputName="password" type="password" pattern=".{8,}"/>
@@ -38,20 +33,20 @@ const { createAccount, logIn } = useAuth()
 const name = defineModel<string>("name")
 const email = defineModel<string>("email")
 const password = defineModel<string>("password")
-const emailError = ref<string>("")
+const errorText = ref<string>("")
 const passwordError = ref<string>("")
 
 function forgotPass() {
-    if (email.value == undefined || !email.value.match(/.+@.+\..+/)) return emailError.value = "Invalid email."
+    if (email.value == undefined || !email.value.match(/.+@.+\..+/)) return errorText.value = "Invalid email."
 }
 
 async function confirm() {
-    if (email.value == undefined || !email.value.match(/.+@.+\..+/)) return emailError.value = "Invalid email."
-    if (password.value == undefined || password.value.length < 8) return passwordError.value = "Password is not long enough"
+    if (email.value == undefined || !email.value.match(/.+@.+\..+/)) return errorText.value = "Invalid email."
+    if (password.value == undefined || password.value.length < 8) return errorText.value = "Password is not long enough"
 
     if (props.login) {
         try {
-            emailError.value = ""
+            errorText.value = ""
             const user = await logIn(email.value!, password.value!)
             navigateTo("")
         } catch (e) {            
@@ -59,17 +54,17 @@ async function confirm() {
                 console.log(e.code)
                 switch (e.code) {
                     case AuthErrorCodes.INVALID_EMAIL:
-                        emailError.value = "Invalid email."
+                        errorText.value = "Invalid email."
                         break;
                     case AuthErrorCodes.INVALID_PASSWORD:
                     case AuthErrorCodes.INVALID_LOGIN_CREDENTIALS:
-                        emailError.value = "Invalid login details."
+                        errorText.value = "Invalid login details."
                         break;
                     case AuthErrorCodes.TOO_MANY_ATTEMPTS_TRY_LATER:
-                        emailError.value = "Too many attempts, try again later."
+                        errorText.value = "Too many attempts, try again later."
                         break;
                     default:
-                        emailError.value = "Something broke, try again, and/or contact support."
+                        errorText.value = "Something broke, try again, and/or contact support."
                         break;
                 }
             }
@@ -78,24 +73,27 @@ async function confirm() {
     }
 
     else {
+        if (name.value == undefined || !name.value.match(/\S.*/)) return errorText.value = "Name cannot be empty."
         try {
+            errorText.value = ""
             const user = await createAccount(name.value!, email.value!, password.value!)
+            navigateTo("")
         } catch (e) {            
             if (e instanceof FirebaseError) {
                 console.log(e.code)
                 switch (e.code) {
                     case AuthErrorCodes.INVALID_EMAIL:
-                        emailError.value += "Invalid email."
+                        errorText.value += "Invalid email."
                         break;
                     case AuthErrorCodes.CREDENTIAL_ALREADY_IN_USE:
                     case AuthErrorCodes.EMAIL_EXISTS:
-                        emailError.value += "This email is already in use."
+                        errorText.value += "This email is already in use."
                         break;
                     case AuthErrorCodes.TOO_MANY_ATTEMPTS_TRY_LATER:
-                        emailError.value += "Too many attempts, try again later."
+                        errorText.value += "Too many attempts, try again later."
                         break;
                     default:
-                        emailError.value += "Something broke, try again, and/or contact support."
+                        errorText.value += "Something broke, try again, and/or contact support."
                         break;
                 }
             }
@@ -104,13 +102,13 @@ async function confirm() {
 }
 
 watch(email, async (newEmail, oldEmail) => {
-    if (newEmail == "" || newEmail != undefined && newEmail.match(/.+@.+\..+/)) emailError.value = ""
-    if (newEmail != oldEmail && emailError.value != "")  emailError.value = ""
+    if (newEmail == "" || newEmail != undefined && newEmail.match(/.+@.+\..+/)) errorText.value = ""
+    if (newEmail != oldEmail && errorText.value != "")  errorText.value = ""
 })
 
 watch(password, async (newPass, oldPass) => {
-    if (newPass == "" || newPass != undefined && newPass.length >= 8) passwordError.value = ""
-    if (newPass != oldPass && emailError.value != "")  emailError.value = ""
+    if (newPass == "" || newPass != undefined && newPass.length >= 8) errorText.value = ""
+    if (newPass != oldPass && errorText.value != "")  errorText.value = ""
 })
 
 </script>
